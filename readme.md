bda30534f5ce63a4ffddd5528550ebbbf3e52e19
Melody Kittycat <catgirl@softmodd.ing>
2026-03-30 22:21:33 +0100

    Added support for infinite annptators because config file

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Niri screenshot tools
+a quick and dirty app that lets you use the native niri screenshotter with annotators like satty or swappy. it also has rudimentary uploader features, intended for use with something like copyparty.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::process::Command;
 
 use niri_ipc::{Event, Request, Response, socket::Socket};
+use serde::Deserialize;
 
-fn annotate(path: String) {
-    let mut annotator = Command::new("satty");
-    match annotator.arg("--filename").arg(path).output() {
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct Config {
+    annotator: Annotator,
+    uploader: Uploader,
+}
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct Annotator {
+    command: String,
+    enabled: bool,
+    auto: bool,
+}
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct Uploader {
+    url: String,
+    enabled: bool,
+}
+fn annotate(path: String, config: &Annotator) {
+    let mut annotator = Command::new("sh");
+    let command = config.command.replace("%path%", &path);
+    match annotator.arg("-c").arg(command).output() {
         Ok(out) => {
             println!("{out:?}")
         }
@@ -13,7 +34,14 @@ fn annotate(path: String) {
         }
     }
 }
+
 fn main() -> std::io::Result<()> {
+    let config = std::fs::read_to_string(
+        pathexpand::expand("${XDG_CONFIG_HOME:~/.config}/niri-screenshot-tools/config.toml")
+            .unwrap(),
+    )
+    .expect("Please write a config file (temporary)");
+    let config = toml::from_str::<Config>(&config).expect("unable to parse config");
     let mut socket = Socket::connect()?;
 
     let reply = socket.send(Request::EventStream)?;
@@ -21,7 +49,7 @@ fn main() -> std::io::Result<()> {
         let mut read_event = socket.read_events();
         while let Ok(event) = read_event() {
             if let Event::ScreenshotCaptured { path: Some(path) } = event {
-                annotate(path);
+                annotate(path, &config.annotator);
             }
         }
     }
